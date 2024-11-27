5578a282d3242bffa6ea47e4457899be6625008d
Ichurri <santiagoiturrivargas04@gmail.com>
2024-11-27 13:44:56 -0400

    add .env variables

--- server/server.js
+++ server/server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config()
 
-// Inicializa Express
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/students', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
@@ -19,5 +19,4 @@ mongoose.connect('mongodb://127.0.0.1:27017/students', {
 const studentRoutes = require('./routes/students.routes');
 app.use('/students', studentRoutes);
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(process.env.PORT, () => console.log(`Server running on http://localhost:${process.env.PORT}`));
