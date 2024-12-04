f4f289aa2e6fdae9954633a7c7c343b9f6f2498c
Ichurri <santiagoiturrivargas04@gmail.com>
2024-12-04 01:19:03 -0400

    change mongo localhost

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
     port: process.env.PORT || 5000, 
-    mongoURI: process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/students',
+    mongoURI: process.env.MONGO_URI || 'mongodb://localhost:27017/students',
     frontendUrl: process.env.FRONTEND_URL || 'http://localhost:5173'
   };
   
