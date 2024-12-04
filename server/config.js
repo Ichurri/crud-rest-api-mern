110f90b4956c97031263e9511b8deda6d1c196de
Ichurri <santiagoiturrivargas04@gmail.com>
2024-12-03 23:54:15 -0400

    change env imports

---
+++ server/config.js
@@ -0,0 +1,8 @@
+require('dotenv').config(); 
+
+module.exports = {
+    port: process.env.PORT || 5000, 
+    mongoURI: process.env.MONGO_URI,
+    frontendUrl: process.env.FRONTEND_URL || 'http://localhost:5173'
+  };
+  
--- server/server.js
+++ server/server.js
@@ -2,21 +2,30 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const config = require('./config');
 require('dotenv').config()
 
 const app = express();
 
-app.use(cors());
+const port = config.port;
+const mongoURI = config.mongoURI;
+const frontendUrl = config.frontendUrl;
+
+app.use(cors
+  ({
+    origin: frontendUrl,
+  })
+);
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/students', {
+mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('Error connecting to MongoDB:', err));
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
 
 const studentRoutes = require('./routes/students.routes');
 app.use('/students', studentRoutes);
 
-app.listen('mongodb://127.0.0.1:27017/students', () => console.log(`Server running`));
+app.listen(port, () => console.log(`Server running`));
