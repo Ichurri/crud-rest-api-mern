6330769b634b776e8b6a67a0552cccee2188e9e1
Ichurri <santiagoiturrivargas04@gmail.com>
2024-12-04 22:35:44 -0400

    test to env

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
     port: process.env.PORT || 5000, 
-    mongoURI: process.env.MONGO_URI || '',
+    mongoURI: process.env.MONGO_URI,
     frontendUrl: process.env.FRONTEND_URL || ''
   };
   
